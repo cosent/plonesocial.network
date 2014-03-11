164f88c2627460945fc89db7c99f4167a1473958
hvelarde <hector.velarde@gmail.com>
2014-03-11 12:16:05 -0300

    Fix package distribution
    - Add classifiers
    - Add dependencies
    - Rename documentation to .rst to reflect reStructuredText formatting
    - Fix license version number to GPLv2 as stated on included LICENSE.GPL file
    - Add MANIFEST.in file

---
+++ CONTRIBUTORS.rst
@@ -0,0 +1,8 @@
+Contributors
+============
+
+* Guido Stevens, Author
+* Leonardo J. Caballero G.
+* Thomas Desvenain
+* Maarten Kling
+* Hector Velarde
--- setup.py
+++ setup.py
@@ -5,12 +5,9 @@ version = '0.6.0dev'
 long_description = (
     open('README.rst').read()
     + '\n' +
-    'Contributors\n'
-    '============\n'
+    open('CONTRIBUTORS.rst').read()
     + '\n' +
-    open('CONTRIBUTORS.txt').read()
-    + '\n' +
-    open('CHANGES.txt').read()
+    open('CHANGES.rst').read()
     + '\n')
 
 setup(
@@ -19,23 +16,43 @@ setup(
     description="Personal profile + follow/unfollow functionality "
     "for the Plonesocial suite",
     long_description=long_description,
-    # Get more strings from
-    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=["Programming Language :: Python", ],
+    classifiers=[
+        #'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Plone :: 4.2',
+        'Framework :: Plone :: 4.3',
+        'Framework :: Plone',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     keywords='plone, zope, socbiz, plonesocial',
     author='Guido Stevens',
     author_email='guido.stevens@cosent.net',
     url='http://github.com/cosent/plonesocial.network',
-    license='gpl',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['plonesocial'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['setuptools',
-                      'Plone',
-                      # -*- Extra requirements: -*-
-                      ],
-    extras_require={'test': ['plone.app.testing']},
+    install_requires=[
+        'plone.app.layout',
+        'Products.CMFCore',
+        'Products.CMFPlone >=4.2',
+        'Products.GenericSetup',
+        'setuptools',
+        'zope.component',
+        'zope.i18nmessageid',
+        'zope.interface',
+    ],
+    extras_require={
+        'test': [
+            'plone.app.testing',
+            'unittest2',
+        ],
+    },
     entry_points="""
       # -*- Entry points: -*-
           [z3c.autoinclude.plugin]
